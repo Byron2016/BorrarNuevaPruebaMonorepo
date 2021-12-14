57b69d1212269e5a0d2bdb0405f52608bba55e73
Byron 2016 <bgva2005@yahoo.com>
2021-12-13 20:11:09 -0500

    Agregado frontEnd y componentes

---
+++ frontend/packages/components/.storybook/main.js
@@ -0,0 +1,31 @@
+async function supportCssModules(config) {
+  // console.log('1=================================')
+  // console.log('>>>config', config.module.rules)
+  // console.log('1=================================')
+
+  config.module.rules.find((rule) => rule.test.toString() === "/\\.css$/").exclude = /\.module\.css$/;
+
+  config.module.rules.push({
+    test: /\.module\.css$/,
+    use: [
+      "style-loader",
+      {
+        loader: "css-loader",
+        options: {
+          modules: true,
+        },
+      },
+    ],
+  });
+
+  return config;
+}
+
+module.exports = {
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+
+  // FIXME: Support CSS Modules for Storybook
+  webpackFinal: supportCssModules,
+};
---
+++ frontend/packages/components/src/components/SomeView/SomeView.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SomoView from "./SomeView";
+
+export default {
+  component: SomoView,
+  title: "Atoms/SomoView",
+};
+
+const Template = (args) => <SomoView />;
+
+export const Default = Template.bind({});
