30585491ef58ffc0271cf2e84c46c9ea890ba26c
Byron 2016 <bgva2005@yahoo.com>
2021-12-13 22:05:25 -0500

    intentar commint para fun lint stage

--- frontend/packages/components/src/components/SomeView/SomeView.js
+++ frontend/packages/components/src/components/SomeView/SomeView.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './SomeView.module.css'
 // import "./style.css";
-
+const c = 5
 const SomeView = () => <div className={styles.root}>Some shared component!</div>
 
 // const SomeView = () => <div className="root">Some shared component!</div>;
