72224f96c276ee6699c5fcc4d549a2c475fc5d0d
Byron 2016 <bgva2005@yahoo.com>
2021-12-13 21:25:15 -0500

    Agregar prettier

--- frontend/packages/components/.eslintrc.js
+++ frontend/packages/components/.eslintrc.js
@@ -1,23 +1,22 @@
+// const RULES = {
+//   OFF: "off",
+//   WARN: "warn",
+//   ERROR: "error",
+// };
 module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
+  extends: ['plugin:react/recommended', 'standard', 'eslint-config-prettier'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
+  plugins: ['react'],
+  rules: {},
 }
